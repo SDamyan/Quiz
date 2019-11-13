ca591aa9f56c2626aa497b5f0d3b6f66f46b5fad
Sara Elin <sedamyan@gmail.com>
2019-11-13 15:22:28 -0600

    added 4 files: index, css, 2js. Added basic html and css styling

---
+++ questions.js
@@ -0,0 +1,30 @@
+//This page I separated for just the questions to present cleaner code
+var questions = [
+    {
+      title: "What neighborhood is Wrigley Field in?",
+      choices: ["Lakeview", "Pilsen", "Logan Square", "Wrigleyville"],
+      answer: "Wrigleyville"
+    },
+    {
+      title: "What does a Chicogan NOT put on a hot dog?",
+      choices: ["ketchup", "pickle", "onion", "relish"],
+      answer: "ketchup"
+    },
+    {
+        title: "Which of these movies is NOT based in Chicago?",
+        choices: ["Home Alone", "Batman: The Dark Knight", "Backdraft", "Crash"],
+        answer: "Crash"
+      },
+      {
+        title: "What holiday does the city dye the river?",
+        choices: ["Christmas", "Valentine's Day", "St. Patrick's Day", "Forth of July"],
+        answer: "St. Patrick's Day"
+      },
+      {
+        title: "What lake boarders Chicago?",
+        choices: ["Lake Superior", "Lake Michagin", "Lake Ontario", "Lake Huron"],
+        answer: "Lake Michagin"
+      },
+          
+  ];
+  
