f4f8a959a4d5d51df37f6392d98ef80bc5efb2f9
Sara Elin <sedamyan@gmail.com>
2019-11-15 13:52:52 -0600

    added in high score page display

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
+//Created const variables to link my html
 const startButton = document.getElementById ("start-btn");
 const nextButton = document.getElementById ("next-btn");
 const highScoresButton = document.getElementById ("high-score-btn");
+const enterScoresButton = document.getElementById ("enter-score-btn");
+
 const openingText = document.getElementById ("intro");
 const questionContainerElement = document.getElementById ("question-container");
 const questionElement = document.getElementById ("question");
 const answerButtonsElement = document.getElementById ("answer-buttons");
-let highScore = 0;
+const initialsEnter = document.getElementById ("high-score-page")
 
+//Created empty let variables, to allow for changes throughout the program
+let highScore = 0;
 let shuffledQuestions;
 let currentQuestionIndex;
 
+//Adding in event listeners to trigger the start of the game, next question, or highscore page
 startButton.addEventListener ("click", startGame)
 nextButton.addEventListener("click", () => {
     currentQuestionIndex++
     setNextQuestion()
 })
 highScoresButton.addEventListener ("click", highScorePage)
+enterScoresButton.addEventListener ("click", enterScoreDisplay)
 
+
+//Function for starting the game. Hiding initial start screen and removing hide from question screen 
 function startGame () {
     startButton.classList.add("hide")
     openingText.classList.add("hide")
+//added in a shuffle sort, so that each time the user takes the quiz, the questions will appear in a random order
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     questionContainerElement.classList.remove("hide")
     setNextQuestion ()
 }
 
+
+//Function to reset the screen and to trigger to show the next question.
 function setNextQuestion () {
     resetState()
     showQuestion (shuffledQuestions[currentQuestionIndex])
 }
 
+//Function to set up the page to open the next questions, upon clicking the next button
 function showQuestion(question) {
     questionElement.innerText = question.question
     question.answers.forEach(answer => {
@@ -45,6 +58,7 @@ function showQuestion(question) {
     })
 }
 
+//Function to reset the page
 function resetState() {
     clearStatusClass(document.body)
     nextButton.classList.add("hide")
@@ -53,7 +67,8 @@ function resetState() {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild)
     }
 }
-    
+
+//Function for when the user selects an answer
 function selectAnswer (e){
     const selectedButton = e.target
     const correct = selectedButton.dataset.correct;
@@ -67,7 +82,7 @@ function selectAnswer (e){
         //set the data set on whether or not that was a correct answer
         setStatusClass(button, button.dataset.correct)
     })
-    //corrected, wrong formatting 
+    //if else statement for running the next question, or showing the highscore button 
     if (shuffledQuestions.length > currentQuestionIndex + 1) {
         nextButton.classList.remove("hide")
     } else {
@@ -77,7 +92,7 @@ function selectAnswer (e){
     }
 }
     
-
+//Function to denote if the user has selected the correct of wrong answer
 function setStatusClass(element, correct) {
     clearStatusClass(element)
     if (correct) {
@@ -87,17 +102,32 @@ function setStatusClass(element, correct) {
     }
 }
 
+//Function to remove style formatting for correct or wrong
 function clearStatusClass(element) {
     element.classList.remove("correct")
     element.classList.remove("wrong")
 }
 
+//Function to open the high score page
 function highScorePage () {
     questionContainerElement.classList.add("hide")
-    
+    highScoresButton.classList.add("hide")
+    enterScoresButton.classList.remove("hide")
+    initialsEnter.classList.remove("hide")
+
+
+//NEED TO ADD: a way to show highscores on the screen with initials, stored in localstorage
+//and reset button to bring the user back to the beginning of the quiz
 }
 
 
+function enterScoreDisplay () {
+
+}
+
+
+//Added questions in a slightly different format then the one suggested, 
+//Here the value of correct is already equalled to true or false for easier data tracking
 const questions = [
     {
       question: "What neighborhood is Wrigley Field in?",
